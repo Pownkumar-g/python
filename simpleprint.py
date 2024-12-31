3cc743ee3b817f5e960c46a3fc106dfadfb3fcaf
pownkumar-g <pownkumarofficial@gmail.com>
2024-12-31 21:07:53 +0530

    Simple Print Statement

--- simpleprint.py
+++ simpleprint.py
@@ -1,1 +1,2 @@
+#In python we use simple print Keyword to print any statement under double quotation 
 print("Hello World !")
---
+++ variables.py
@@ -0,0 +1,16 @@
+#The values for the variable's a ,b ,c  is defined by the programmer 
+a=10
+b=20
+c=a+b
+#In this m ,n the value for the variable is given by the user's
+m=int(input("Enter any integer value for variable m :"))
+n=float(input("Enter any float value for variable n:"))
+x,y,z=10,20,30
+s=d=p=25
+print("The value of the variable m is :" ,m)
+print("The value of the variable n is :" ,n)
+print("The value of the variable a is :" ,a)
+print("The value of the variable b is :" ,b)
+print("The value of the variable c is :" ,c)
+print("The value of the variable's x,y,z is :" ,x, y, z)
+print("The value of the variable's s,d,p is :" ,s, d, p)
