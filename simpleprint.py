6d5fff4fa11679ab4dfe56924b090c010708c70e
pownkumar-g <pownkumarofficial@gmail.com>
2024-12-31 21:44:30 +0530

    The Print Statement's

--- simpleprint.py
+++ simpleprint.py
@@ -1,6 +1,8 @@
 #In python we use simple print Keyword to print any statement under double quotation 
 print("Hello World !")
 print("My","name","is","Amit")
+#The sep keyword is used to separate the print statement at the comma place
 print("My","name","is","Amit",sep='***')
+#The end keyword is used to print the end value in the print statement at the end comma
 print("What is you name " ,end='?')
 print("My name is Amit")
