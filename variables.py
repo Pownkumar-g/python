5c872cc9a19a7efffd051d1d70d6d1c1a8e0e283
pownkumar-g <pownkumarofficial@gmail.com>
2024-12-31 21:38:49 +0530

    The Variable Declaration

--- variables.py
+++ variables.py
@@ -2,9 +2,12 @@
 a=10
 b=20
 c=a+b
+name='Pown Kumar G'
+Brother="Arun Kumar G"
 #In this m ,n the value for the variable is given by the user's
 m=int(input("Enter any integer value for variable m :"))
 n=float(input("Enter any float value for variable n:"))
+Place=str(input("Enter you location "))
 x,y,z=10,20,30
 s=d=p=25
 print("The value of the variable m is :" ,m)
@@ -14,3 +17,15 @@ print("The value of the variable b is :" ,b)
 print("The value of the variable c is :" ,c)
 print("The value of the variable's x,y,z is :" ,x, y, z)
 print("The value of the variable's s,d,p is :" ,s, d, p)
+print("My Name is = ",name)
+print("My Brother name is = " ,Brother)
+#To Assign one variable value to another variable
+value=10
+new_value=value
+print(value)
+print(new_value)
+#To print Dynamic Typing
+g=10
+print(g)
+g="New value"
+print(g)
