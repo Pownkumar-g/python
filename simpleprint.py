c00916975ed3f707a9c492bbe597a9751c17f841
pownkumar-g <pownkumarofficial@gmail.com>
2024-12-31 21:36:56 +0530

    The Print Statement's

--- simpleprint.py
+++ simpleprint.py
@@ -1,2 +1,6 @@
 #In python we use simple print Keyword to print any statement under double quotation 
 print("Hello World !")
+print("My","name","is","Amit")
+print("My","name","is","Amit",sep='***')
+print("What is you name " ,end='?')
+print("My name is Amit")
