69131edd514005b70caf953af328e7ffdd68a722
pownkumar-g <pownkumarofficial@gmail.com>
2025-01-01 21:11:43 +0530

    The Print Statement's

--- simpleprint.py
+++ simpleprint.py
@@ -1,8 +1,25 @@
 #In python we use simple print Keyword to print any statement under double quotation 
+
 print("Hello World !")
 print("My","name","is","Amit")
+
 #The sep keyword is used to separate the print statement at the comma place
+
 print("My","name","is","Amit",sep='***')
+
 #The end keyword is used to print the end value in the print statement at the end comma
+
 print("What is you name " ,end='?')
 print("My name is Amit")
+
+#To print an variable type
+ 
+a=2+4j
+print(type(a))
+
+#To print an paticular character  of a word
+
+s='hello world'
+print(s[1])
+print(s[3])
+print(s[-1])
--- variables.py
+++ variables.py
@@ -1,10 +1,13 @@
 #The values for the variable's a ,b ,c  is defined by the programmer 
+
 a=10
 b=20
 c=a+b
 name='Pown Kumar G'
-Brother="Arun Kumar G"
+Friend="Amit G"
+
 #In this m ,n the value for the variable is given by the user's
+
 m=int(input("Enter any integer value for variable m :"))
 n=float(input("Enter any float value for variable n:"))
 Place=str(input("Enter you location "))
@@ -18,14 +21,23 @@ print("The value of the variable c is :" ,c)
 print("The value of the variable's x,y,z is :" ,x, y, z)
 print("The value of the variable's s,d,p is :" ,s, d, p)
 print("My Name is = ",name)
-print("My Brother name is = " ,Brother)
+print("My Brother name is = " ,Friend)
+
 #To Assign one variable value to another variable
+
 value=10
 new_value=value
 print(value)
 print(new_value)
+
 #To print Dynamic Typing
+
 g=10
 print(g)
 g="New value"
 print(g)
+
+#To print an variable type
+ 
+a=2+4j
+print(type(a))
